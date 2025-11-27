9fe3497d088a520e6415e94bd4fb56492e05cc2d
11968-ship-it <11968@holbertonstudents.com>
2025-11-27 14:34:47 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 
+
 print(str[37:64] + str[104:109] + str[:6])
