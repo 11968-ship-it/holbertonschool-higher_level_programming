7013738c85bbb0b44006730169701eecc8dcacfa
11968-ship-it <11968@holbertonstudents.com>
2025-11-29 23:45:25 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python object-oriented programming with very clear syntax"
+str = "Python, object-oriented programming with very clear syntax"
 
 print(str[7:22] + str[23:40] + str[:6])
