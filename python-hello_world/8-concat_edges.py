32d6573007206efe25ea34f509ded36e5f42e03b
11968-ship-it <11968@holbertonstudents.com>
2025-11-30 18:12:14 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 #object-oriented programming with Python
-print(str[39:67] + str[107:112] + str[63:74] + str[:6])
+print(str[39:67] + str[107:112] + str[:6])
