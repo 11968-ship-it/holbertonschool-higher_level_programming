81e781208dfe68a6df02ee2ba4fc95c2a925c957
11968-ship-it <11968@holbertonstudents.com>
2025-11-30 18:03:26 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print(type(int).__name__ + '-' + type(type).__name__ + ' ' +
-      type(list).__name__ + ' ' + type(dict).__name__ + ' ' +
-      type(str).__name__)
+print((print.__doc__[13:19] + '-' + (str.__doc__[10:18] + ' ') +
+       (int.__doc__[32:43] + ' ') + (list.__doc__[7:13] + ' ') +
+       type.__name__))
