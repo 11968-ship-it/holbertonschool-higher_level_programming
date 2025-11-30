e95d114083df6ac648f9827d470952182a05eb25
11968-ship-it <11968@holbertonstudents.com>
2025-11-30 18:41:44 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an, object-oriented programming language that combines remarkable power with very"
-print(str[14:42] + str[82:87] + str[:6])
+str = "Python is an, object-oriented programming language that with very"
+print(str[14:42] + str[56:61] + str[:6])
