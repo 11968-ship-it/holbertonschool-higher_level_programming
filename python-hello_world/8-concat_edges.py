620818b45f7efda51ba9cde1e635ab6ecffa34ae
11968-ship-it <11968@holbertonstudents.com>
2025-11-30 18:08:15 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:55] + '-' + str[39:47] + ' ' + str[63:74] + ' ' + str[:6])
+print(str[39:55] + str[39:47] + str[63:74] + str[:6])
