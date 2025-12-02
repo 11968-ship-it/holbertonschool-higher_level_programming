474956c2c75d0f4dff388962968f2ddd6fe1397b
11968-ship-it <11968@holbertonstudents.com>
2025-12-02 19:05:08 +0300

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for m in range(10):
     for j in range(m + 1, 10):
         if m == 8 and j == 9:
-            print(f"{m}{j}")
+            print(f"{}{}".format(m, j))
         else:
-           print(f"{m}{j}", end=", ")
+           print(f"{}{}".format(m, j), end=", ")
