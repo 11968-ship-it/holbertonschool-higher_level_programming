0124db56a554de0b1a321b804a02ed3dee966aa4
11968-ship-it <11968@holbertonstudents.com>
2025-12-03 13:59:00 +0300

    Update 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """Retrieve an element from a list at a specific index.
-    
+
     Args:
         my_list (list): The list to retrieve the element from.
         idx (int): The index of the element to retrieve.
-    
+
     Returns:
         The element at index idx if valid, otherwise None.
     """
