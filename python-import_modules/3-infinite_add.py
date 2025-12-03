58ab1b072dce877427546bc63d818e919934de23
11968-ship-it <11968@holbertonstudents.com>
2025-12-03 13:38:16 +0300

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
-    # Sum all command-line arguments casted to integers
     total = sum(int(arg) for arg in sys.argv[1:])
     print(total)
+
 
 if __name__ == "__main__":
     main()
