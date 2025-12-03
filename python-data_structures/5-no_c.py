0cfde8accab03256f8f8f1696a75eca26ce411d4
11968-ship-it <11968@holbertonstudents.com>
2025-12-03 14:06:19 +0300

    Update 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+def no_c(my_string):
+    """Return a copy of my_string without 'c' and 'C' characters."""
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
