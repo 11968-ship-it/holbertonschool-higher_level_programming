b68bbfc3b98cdd55f3202908ec7cb7384256aa15
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 11:50:34 +0300

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    L = my_list[0]
+    # for loop method
+    #for num in my_list:
+    #    if num > L:
+    #        L = num
+    # while method
+    i = 1
+    while i < len(my_list):
+        if my_list[i] > L:
+            L = my_list[i]
+        i += 1
+    return L
