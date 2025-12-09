63728553a6c5ceb1c5e89d606b32a3bd4d52850c
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:34:01 +0300

    Update 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,26 @@
-
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides elements from my_list_1 by my_list_2 element-wise safely.
+    Returns a list of length list_length with results or 0 on error.
+    Prints errors as specified."""
+    result = []
+    for i in range(list_length):
+        div = 0
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+                raise TypeError
+            div = a / b
+        except IndexError:
+            print("out of range")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except TypeError:
+            print("wrong type")
+            div = 0
+        finally:
+            result.append(div)
+    return result
