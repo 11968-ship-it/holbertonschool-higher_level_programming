f9f5d1efdb0fbb1ca6c900096b4621df72df8318
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:12:40 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list and return the count."""
+    """Print up to x elements of a list and return the number of elements printed."""
     count = 0
     for i in range(x):
         try:
