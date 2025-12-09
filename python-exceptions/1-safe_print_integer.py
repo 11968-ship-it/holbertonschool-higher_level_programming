eab8dbab5bf842b69b7108b1cc77aaedc7f2b4a2
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:19:13 +0300

    Update 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Print an integer using '{:d}'.format().
+    Returns True if value is an integer and printed correctly,
+    otherwise returns False."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
