5eb492b46b7dd424c65f2e4e380f2dd66cdeb8a0
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:10:45 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 def safe_print_list(my_list=[], x=0):
     """Print x elements of a list and return the count."""
     count = 0
-    for m in range(x):
+    for i in range(x):
         try:
-            print(my_list[m], end="")
+            print(my_list[i], end="")
             count += 1
         except IndexError:
             break
