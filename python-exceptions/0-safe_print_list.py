5a86c819ae9fc3ea39ab35ff2d88e1298de09a42
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:02:34 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for m in range(x):
+    for i in range(x):
         try:
-            print(my_list[m], end="")
+            print(my_list[i], end="")
             count += 1
         except IndexError:
             break
