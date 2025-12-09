7d0526722cff289cfc4f8ff5b9979f27f743785e
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 17:53:33 +0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,9 @@
-
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+        except IndexError:
+            break
+    print()
+    return count  
