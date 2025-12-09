1df4cba630cc6282a11063d78b1e86aff2756270
11968-ship-it <11968@holbertonstudents.com>
2025-12-09 18:26:25 +0300

    Update 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides a by b, prints the result inside finally, returns the result or None."""
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
