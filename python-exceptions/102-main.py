d268df526509fdf10c3770acbe00750b14db0875
11968-ship-it <11968@holbertonstudents.com>
2025-12-10 13:10:41 +0300

    Create 102-main.py

---
+++ python-exceptions/102-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+# Test cases
+print(magic_calculation(2, 3))  # Expected: 12
+print(magic_calculation(0, 3))  # Expected: 3
+print(magic_calculation(5, 2))  # Expected: 37.5
+print(magic_calculation(1, 4))  # Expected: 17.0
