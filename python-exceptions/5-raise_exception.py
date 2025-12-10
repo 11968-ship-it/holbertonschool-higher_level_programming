d3b65713be5349fd1c114004ec61b03e52d9d5f5
11968-ship-it <11968@holbertonstudents.com>
2025-12-10 12:58:11 +0300

    Update 5-raise_exception.py

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a function that raises a TypeError exception.
+"""
 
+def raise_exception():
+    """
+    Raises a TypeError exception when called.
+    """
+    raise TypeError
