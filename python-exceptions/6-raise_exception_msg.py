eae339c11019b9fa9683ddf95404bf12ef00a1bf
11968-ship-it <11968@holbertonstudents.com>
2025-12-10 13:01:30 +0300

    Update 6-raise_exception_msg.py

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a function that raises a NameError exception
+with a custom message.
+"""
 
+
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError exception with the given message.
+
+    Args:
+        message (str): The message to be used in the exception.
+    """
+    raise NameError(message)
