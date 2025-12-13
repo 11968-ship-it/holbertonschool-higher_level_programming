6e706f0cdfd0c5db236adfbd3bd6be74bcfc08b4
11968-ship-it <11968@holbertonstudents.com>
2025-12-13 12:23:26 +0300

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square with a private attribute size."""
 
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square with a private size."""
+        self.__size = size
