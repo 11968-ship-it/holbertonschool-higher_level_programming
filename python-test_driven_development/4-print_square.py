f75edadf7750aa6f99517e5215890f3e71564d67
11968-ship-it <11968@holbertonstudents.com>
2025-12-13 11:12:58 +0300

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+"""
+4-print_square module
 
+This module contains a function that prints a square with the character #.
+"""
+
+def print_square(size):
+    """Prints a square of # characters with the given size."""
+    # Check type
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    # Check value
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Print square
+    for _ in range(size):
+        print("#" * size)
