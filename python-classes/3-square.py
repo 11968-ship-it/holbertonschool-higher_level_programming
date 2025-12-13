ba071728b73a5a08debcbacaa42b3eb72ff7c415
11968-ship-it <11968@holbertonstudents.com>
2025-12-13 12:27:43 +0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+"""Defines a square with private size and area method."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square with optional size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
 
