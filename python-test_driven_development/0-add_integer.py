861a5f7ffc0bd5c25ec4ec3b1ce219b3c4e69c75
11968-ship-it <11968@holbertonstudents.com>
2025-12-13 10:47:18 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,12 +6,12 @@ This module contains a function that adds two integers.
 """
 
 def add_integer(a, b=98):
+    """Adds two integers.
+
+    Returns the addition of a and b."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    # Convert to int *after* type check
-    # This handles standard floats by truncating them
-    # and large ints without overflow problems.
+
     return int(a) + int(b)
