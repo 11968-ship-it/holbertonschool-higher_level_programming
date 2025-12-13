9f1b6a9a2cbcdea3ec4e4bdabf841112cacab702
11968-ship-it <11968@holbertonstudents.com>
2025-12-13 12:26:12 +0300

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square with private size and validation."""
 
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square with optional size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
