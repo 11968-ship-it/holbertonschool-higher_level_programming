038b989247a031cc4ede23cd7f462610607db8c5
11968-ship-it <11968@holbertonstudents.com>
2025-12-18 14:01:28 +0300

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that defines a function inherits_from to check if an
+object is an instance of a class that inherited from a
+specified class.
+"""
 
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from a_class, otherwise returns False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
