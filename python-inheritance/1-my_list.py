f4227db533137dcf9162beeaec4c24f3518d6dfc
11968-ship-it <11968@holbertonstudents.com>
2025-12-18 13:28:35 +0300

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that definesoka MyList, a list subclass with a print_sorted method
+"""
 
+class MyList(list):
+    """
+    This class prints a sorted list
+    """
+  
+    def print_sorted(self):
+        """
+        Prints a sorted copy of the list without modifying the original list
+        """
+        print(sorted(self))
