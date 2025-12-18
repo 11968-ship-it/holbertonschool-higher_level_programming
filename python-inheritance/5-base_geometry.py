eaaeadb6fb1b292c926a0ccd49a9fd110f10d512
11968-ship-it <11968@holbertonstudents.com>
2025-12-18 14:05:46 +0300

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty class BaseGeometry.
+"""
 
+
+class BaseGeometry:
+    """
+    BaseGeometry class: an empty class to serve as a base for geometry.
+    """
+    pass
