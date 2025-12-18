cd7823d97e04fd745209eb116c7e0be7e970ee21
11968-ship-it <11968@holbertonstudents.com>
2025-12-19 01:17:05 +0300

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class that inherits
+from Rectangle. The class implements a square with
+size validation.
+"""
 
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square defined by size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns the string representation of the square.
+        """
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
