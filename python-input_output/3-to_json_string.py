2b3d6d07148dbd7f8ff005752c3f77ceca1245b4
11968-ship-it <11968@holbertonstudents.com>
2025-12-26 17:38:12 +0300

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""Module that converts an object to its JSON string representation."""
 
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object."""
+    return json.dumps(my_obj)
