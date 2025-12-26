84b7611965e769497b2f4ee0a7f5c81d48d9352b
11968-ship-it <11968@holbertonstudents.com>
2025-12-26 17:34:54 +0300

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to append text to a file."""
 
+
+def append_write(filename="", text=""):
+    """Appends a string to a UTF-8 file and returns the character count."""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
