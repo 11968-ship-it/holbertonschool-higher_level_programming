8651562fda43dd03e16d0676cb99b83d695dd26d
11968-ship-it <11968@holbertonstudents.com>
2025-12-26 17:28:03 +0300

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module that defines a function to write a string to a text file."""
+"""Module that defines a function to write text to a file."""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    """Writes a string to a UTF-8 file and returns the character count."""
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
