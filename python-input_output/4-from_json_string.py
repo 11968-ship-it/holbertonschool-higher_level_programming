fc55875c4ce93bb6b8689d6bf3fa22c4a9500b0a
11968-ship-it <11968@holbertonstudents.com>
2025-12-26 17:40:32 +0300

    Update 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""Module that converts a JSON string into a Python object."""
 
+import json
+
+
+def from_json_string(my_str):
+    """Returns a Python object represented by a JSON string."""
+    return json.loads(my_str)
