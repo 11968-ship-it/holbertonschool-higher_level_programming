b349d0f67ab426948e6b24af0443a0daab01fbf5
11968-ship-it <11968@holbertonstudents.com>
2025-12-27 10:50:18 +0300

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description of an object
+for JSON serialization.
+"""
 
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structures
+    of an object for JSON serialization.
+    """
+    return obj.__dict__
