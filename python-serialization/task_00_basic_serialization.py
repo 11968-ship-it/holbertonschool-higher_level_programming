8da080b15889abf02e1b001d31fbb0c363f44ebe
11968-ship-it <11968@holbertonstudents.com>
2025-12-27 12:19:15 +0300

    Update task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3
+"""
+Basic serialization module
+"""
 
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): Python dictionary to serialize.
+        filename (str): Filename of the output JSON file.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+def load_and_deserialize(filename):
+    """
+    Load and deserialize a JSON file into a Python dictionary.
+
+    Args:
+        filename (str): Filename of the input JSON file.
+
+    Returns:
+        dict: Deserialized Python dictionary.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
