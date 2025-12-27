d66958517ddd1a815b2a17db17dd9ec6b98be875
11968-ship-it <11968@holbertonstudents.com>
2025-12-27 11:05:51 +0300

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python3
+"""
+Defines a Student class.
+"""
 
+
+class Student:
+    """
+    Student class with public attributes.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of a Student instance.
+        If attrs is a list of strings, only those attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+        return self.__dict__
