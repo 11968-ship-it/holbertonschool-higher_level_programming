82668677c97a84122fde7e7c2180c2a79a90a7af
11968-ship-it <11968@holbertonstudents.com>
2025-12-27 11:09:47 +0300

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns Pascal's triangle of size n.
+"""
 
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    Pascal's triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(1, i):
+                row.append(last_row[j - 1] + last_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
