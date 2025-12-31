b913b94bdba5c7695b5aa9f57258a840021bb33d
11968-ship-it <11968@holbertonstudents.com>
2025-12-31 17:26:52 +0300

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,1 +1,39 @@
+import requests
+import csv
 
+URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+def fetch_and_print_posts():
+    response = requests.get(URL)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post.get("title"))
+
+
+def fetch_and_save_posts():
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        structured_posts = [
+            {
+                "id": post.get("id"),
+                "title": post.get("title"),
+                "body": post.get("body")
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(
+                file,
+                fieldnames=["id", "title", "body"]
+            )
+            writer.writeheader()
+            writer.writerows(structured_posts)
