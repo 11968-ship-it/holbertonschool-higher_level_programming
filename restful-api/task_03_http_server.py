f2c1a712afa5ddabc343a2b0e29c4502d27ebe12
11968-ship-it <11968@holbertonstudents.com>
2026-01-03 01:38:29 +0300

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 import http.server
 import socketserver
+import json
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!\n")
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"name": "John", "age": 30, "city": "New York"}).endcode())
+        else:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!\n")
 
 
 PORT = 8000
