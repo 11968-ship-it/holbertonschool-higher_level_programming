2ba9d61c5e96631b73113bdd02ddc95cb9697db2
11968-ship-it <11968@holbertonstudents.com>
2026-01-03 02:12:41 +0300

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,16 +5,33 @@ import json
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/data":
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!\n")
+
+        
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write((json.dumps({"name": "John", "age": 30, "city": "New York"}) + "\n").encode())
+            self.wfile.write((json.dumps(data) + "\n").encode())
+            
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK\n")
+
+        elif self.path == "/info":
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write((json.dumps(info) + "\n").encode())
+            
         else:
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
