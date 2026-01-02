c677967fc9b2aece0236417b98f6f604a25d3bfc
11968-ship-it <11968@holbertonstudents.com>
2026-01-03 01:12:30 +0300

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+import http.server
+import socketserver
 
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello, this is a simple API!")
+
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
