c675216b2c094142d41fb00130013be7b8336d0d
11968-ship-it <11968@holbertonstudents.com>
2026-01-03 01:26:57 +0300

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,7 +7,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
+        self.wfile.write(b"Hello, this is a simple API!\n")
 
 
 PORT = 8000
