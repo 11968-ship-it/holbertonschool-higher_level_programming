c9b7bbc69ab6446481d89ad6d9745f5dd888b968
11968-ship-it <11968@holbertonstudents.com>
2026-01-03 11:59:49 +0300

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.keys()))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
