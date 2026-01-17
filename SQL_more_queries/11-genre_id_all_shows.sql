5e22048211b2c63cae83b28bc27ef967651732fb
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 12:01:12 +0300

    Update 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,6 @@
-
+-- A script that lists all TV shows with their genre IDs (shows without genres should show NULL)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+-- join tv_show_genres in a way that includes shows without genres
+ON ...
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
