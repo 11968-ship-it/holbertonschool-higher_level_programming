1c45ba4a94038b82d83df8bf55fe9a038ecc5d7a
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 23:16:40 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,39 @@
+#!/usr/bin/python3
+"""
+Lists all cities from the database hbtn_0e_4_usa
+"""
 
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    # Execute a single query to get city id, city name, state name
+    query = ("SELECT cities.id, cities.name, states.name "
+             "FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "ORDER BY cities.id ASC")
+    cursor.execute(query)
+
+    # Fetch and print results
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close connection
+    cursor.close()
+    db.close()
