e249cf0fced92df72c66e127cd53d9c04b2a75ff
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 23:26:06 +0300

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,47 @@
+#!/usr/bin/python3
+"""
+This script lists all cities of a given state from the database
+hbtn_0e_4_usa.
+Usage: ./5-filter_cities.py <mysql_username> <mysql_password>
+<database_name> <state_name>
+"""
 
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> "
+              "<database_name> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query, (state_name,))
+    results = cursor.fetchall()
+
+    cities = [row[0] for row in results]
+    print(", ".join(cities))
+
+    cursor.close()
+    db.close()
