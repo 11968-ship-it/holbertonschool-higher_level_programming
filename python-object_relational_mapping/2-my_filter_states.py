5bb7f4058efa0b934ceac49f9c86469f129ab8bc
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 23:01:10 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    # Execute query using format() with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+
+    # Fetch and print results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close connection
+    cursor.close()
+    db.close()
