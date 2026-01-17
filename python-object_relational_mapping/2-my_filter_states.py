98ae6c940a937fe232ef6d6f9d0fae7bc000a797
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 23:05:07 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument (case-sensitive).
+Displays all values in the states table where name matches the argument.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,15 +24,15 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = (
-        "SELECT * FROM states WHERE BINARY name = '{}' "
-        "ORDER BY id ASC".format(state_name)
-    )
+    # Case-sensitive exact match using BINARY
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
+    # Fetch and print results
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
+    # Close connection
     cursor.close()
     db.close()
