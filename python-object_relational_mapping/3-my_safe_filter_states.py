7c461b076d3bf8dc2a2ccd0e0ff026421d04a0be
11968-ship-it <11968@holbertonstudents.com>
2026-01-17 23:12:44 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,38 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa where name matches
+the argument (safe from SQL injection)
+"""
 
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    # Safe query using parameterized query (prevents SQL injection)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
