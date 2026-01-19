df3fe74f7773d38dc8e75f9120339bcaefb73b55
11968-ship-it <11968@holbertonstudents.com>
2026-01-19 18:20:27 +0300

    Update 4-cities_by_state.sql

--- python-object_relational_mapping/4-cities_by_state.sql
+++ python-object_relational_mapping/4-cities_by_state.sql
@@ -6,8 +6,7 @@ CREATE TABLE IF NOT EXISTS states (
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"),
-  ("New York"), ("Nevada");
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
 CREATE TABLE IF NOT EXISTS cities ( 
     id INT NOT NULL AUTO_INCREMENT, 
