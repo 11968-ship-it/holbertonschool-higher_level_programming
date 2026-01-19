562f62210f280ec243bd181574ea8993320820cb
11968-ship-it <11968@holbertonstudents.com>
2026-01-19 17:53:15 +0300

    Update 7-model_state_fetch_all.sql

--- python-object_relational_mapping/7-model_state_fetch_all.sql
+++ python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -1,2 +1,4 @@
 -- Insert states
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+INSERT INTO states (name) VALUES (
+  "California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada"
+);
