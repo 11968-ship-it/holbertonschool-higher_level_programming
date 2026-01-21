656cf4f91cbb201058230cac12450fed4ef81fe8
11968-ship-it <11968@holbertonstudents.com>
2026-01-21 16:53:50 +0300

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+"""
+Changes the name of the State object with id = 2 to 'New Mexico'
+in the database hbtn_0e_6_usa using SQLAlchemy ORM.
+"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username, password, database
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state is not None:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
