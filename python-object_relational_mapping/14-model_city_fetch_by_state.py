36f1c98f5179db0c73c45e8a958b64ab288153d9
11968-ship-it <11968@holbertonstudents.com>
2026-01-21 17:01:20 +0300

    Update 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,7 +7,7 @@ Format: <state name>: (<city id>) <city name>
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database),
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(username, password, database),
         pool_pre_ping=True
     )
 
