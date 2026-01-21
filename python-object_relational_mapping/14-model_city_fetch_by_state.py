bbdf192f85219ae15b4ab96f589273d15e090154
11968-ship-it <11968@holbertonstudents.com>
2026-01-21 16:58:28 +0300

    Update 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+sorted by cities.id using SQLAlchemy ORM.
+Format: <state name>: (<city id>) <city name>
+"""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        state = session.query(State).filter(State.id == city.state_id).first()
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
