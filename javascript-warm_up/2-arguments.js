3480c9b51009cb2824a4ea7fdad67e5c2e75d38b
11968-ship-it <11968@holbertonstudents.com>
2026-02-07 04:27:52 +0300

    Create 2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const N = process.argv.length -2
+if (N == 0)
+{
+    console.log('No argument');
+}
+else if (N == 1)
+{
+    console.log('Argument found'); 
+}
+else
+{
+    console.log('Arguments found');
+}
