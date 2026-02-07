a0b4dbda75724f0d92029a89563773d1f9b72ad1
11968-ship-it <11968@holbertonstudents.com>
2026-02-07 04:17:01 +0300

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 const m = 'C is fun';
 const l = 'Python is cool';
 const n = 'JavaScript is amazing';
-console.log(m)
-console.log(l)
-console.log(n)
+console.log(m);
+console.log(l);
+console.log(n);
