adfaf44d3447590b111b0d30ad6d091688866625
11968-ship-it <11968@holbertonstudents.com>
2026-02-07 04:33:33 +0300

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,7 @@
 const N = process.argv.length - 2;
 if (N === 0) {
   console.log('No argument');
-}
-else if (N === 1) {
+} else if (N === 1) {
   console.log('Argument found'); 
 } else {
   console.log('Arguments found');
