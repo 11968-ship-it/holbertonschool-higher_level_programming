42561b94ba32ce8f81efabda987a063ab63189b2
11968-ship-it <11968@holbertonstudents.com>
2026-02-14 20:54:22 +0300

    Update 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Defines a restricted attribute class."""
+
+
 class LockedClass:
-    __slots__ = ("first_name",)
+    """
+    Prevent user from creating new instance attributes
+    except if the new instance attribute is called 'first_name'.
+    """
+    __slots__ = ["first_name"]
